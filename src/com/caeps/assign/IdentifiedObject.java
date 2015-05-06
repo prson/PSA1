3a35678fd3dd8c31865d6ed8c91e0198d21b47da
prson <pratik1909@gmail.com>
2015-05-06 16:38:41 +0200

    Commit 2

---
+++ src/com/caeps/assign/IdentifiedObject.java
@@ -0,0 +1,19 @@
+package com.caeps.assign;
+
+
+public class IdentifiedObject  {
+	public String rdfID;
+	public String name;
+	
+	public IdentifiedObject(String rdfId, String n){
+		rdfID=rdfId;
+		name=n;
+	}
+
+	public String getRdfID() {
+		// TODO Auto-generated method stub
+		return rdfID;
+	}
+
+	
+}
