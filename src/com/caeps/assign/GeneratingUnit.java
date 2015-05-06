c77500f808b781c33021b268d5f0116fec171634
ee97364 <ee97364@gmail.com>
2015-05-06 18:12:31 +0200

    Added generating unit class

---
+++ src/com/caeps/assign/GeneratingUnit.java
@@ -0,0 +1,16 @@
+package com.caeps.assign;
+
+public class GeneratingUnit extends ConductingEquipment{
+
+	public double maxP;
+	public double minP;
+	public EquipmentContainer memberOfEquipmentContainer;
+
+	public GeneratingUnit(String rdfId, String n, double maxPow, double minPow, EquipmentContainer memberOfEquipmentContainer){
+		super(rdfId,n);
+		maxP = maxPow;
+		minP = minPow;
+		this.memberOfEquipmentContainer = memberOfEquipmentContainer;
+	}
+
+}
