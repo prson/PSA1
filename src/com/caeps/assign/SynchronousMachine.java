3d1c9152f35b2b51ba7f4b48e5c1484e911de93a
ee97364 <ee97364@gmail.com>
2015-05-06 18:22:59 +0200

    Added sync machine and generating unit

--- src/com/caeps/assign/SynchronousMachine.java
+++ src/com/caeps/assign/SynchronousMachine.java
@@ -3,13 +3,13 @@ package com.caeps.assign;
 public class SynchronousMachine extends ConductingEquipment{
 
 	public double ratedS;
-	public ThermalGeneratingUnit memberOfGen;
+	public GeneratingUnit memberOfGen;
 	public RegulatingControl regControl;
 	public EquipmentContainer memberOfEquipmentContainer;
 	public BaseVoltage baseVoltage;
 	
 	
-	public SynchronousMachine (String rdfId,String name, double ratedS, ThermalGeneratingUnit memberOfGen, RegulatingControl regControl, EquipmentContainer memberOfEquipmentContainer, BaseVoltage baseVoltage){
+	public SynchronousMachine (String rdfId,String name, double ratedS, GeneratingUnit memberOfGen, RegulatingControl regControl, EquipmentContainer memberOfEquipmentContainer, BaseVoltage baseVoltage){
 		super(rdfId,name);
 		this.ratedS=ratedS;
 		this.memberOfGen=memberOfGen;
