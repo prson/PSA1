49b308590b9b9522d9c0ba0dfdc0e745e422ddad
ee97364 <ee97364@gmail.com>
2015-05-15 16:11:25 +0200

    Added comments to conducting equipment

--- src/com/caeps/systemcomponents/ConductingEquipment.java
+++ src/com/caeps/systemcomponents/ConductingEquipment.java
@@ -2,12 +2,29 @@ package com.caeps.systemcomponents;
 
 import java.util.ArrayList;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ConductingEquipment.
+ */
 public class ConductingEquipment extends Equipment{
 
+	/**
+	 * Instantiates a new conducting equipment.
+	 *
+	 * @param rdfId the rdf id
+	 * @param n the name
+	 */
 	public ConductingEquipment (String rdfId, String n){
 		super(rdfId,n);
 	}
 	
+	/**
+	 * Search for conducting equipment component with given rdfId.
+	 *
+	 * @param ab the ab
+	 * @param rdfId the rdf id
+	 * @return the conducting equipment
+	 */
 	static ConductingEquipment searchConductingEquipment (ArrayList<ConductingEquipment> ab, String rdfId) {
 		ConductingEquipment objectFound = null;
 		for (ConductingEquipment objIt : ab) {
